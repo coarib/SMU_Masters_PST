ecea92d36807398feb1da16a799a3557df38d02a
coarib <32246814+coarib@users.noreply.github.com>
2019-05-13 22:21:38 -0500

    Working app.py file with in-time updates

--- app.py
+++ app.py
@@ -18,63 +18,37 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/translate', methods=['GET'])
-def translate():
-    # Params for model are in here!
-    args = request.args.to_dict()
+def populate_main(my_return_df):
 
-    #button_id = request.form['submit_button']
-    #print(button_id)
-	
-    button_id=''
-    button_id = args['submit_button']
-    print(button_id)
-    #set input values
-    sent = args['fromText']
-    from_language=args['fromLang']
-    to_language=args['toLang']
-    #google_text=args['googleText']
-    #pst_text=args['pstText']
-	
-    google_text=''
-    pst_text=''
-	
-    if (button_id=="usePSTTranslation") :
-        updateTranslations(from_language,sent,to_language,pst_text)
-		
-    if (button_id=="useGoogleTranslation") :
-        updateTranslations(from_language,sent,to_language,google_text)
-	
     import numpy as np
     import pickle
     import sklearn
     from sklearn.feature_extraction.text import CountVectorizer
     from sklearn.feature_extraction.text import TfidfVectorizer
     from sklearn.metrics.pairwise import euclidean_distances
-
-    #set input values
-    sent = args['fromText']
-    from_language=args['fromLang']
-    to_language=args['toLang']
-    
+    import re
+    import warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning) 
+	
     # read python df of languages matching the "from_language" from the appropriate smart-named pkl file
-    pkl_file = open('sentences_'+from_language+'.pkl', 'rb')
+    pkl_file = open('sentences_'+my_return_df['from_language'].values[0]+'.pkl', 'rb')
     from_language_sentences = pickle.load(pkl_file)
     pkl_file.close()
 
-    #create a list of the values to be parsed, using our translatable sentence as index 0
-    sent=sent.strip().lower().replace('(', '').replace(')', '')
+    #Clean input
+    my_return_df.ix[0, 'sent'] = re.sub('[!@#$%;:.?()"\'^\{\}\[\]|\\\/<>=`~*&]', '', my_return_df['orig_sent'].values[0]).strip().lower()
+	
 
+    #create a list of the values to be parsed, using our translatable sentence as index 0
     corpus = [] 
-    corpus.append(sent)
+    corpus.append(my_return_df['sent'].values[0])
 
     #Append the appropriate language values
     corpus= corpus + from_language_sentences['text'].tolist()
 
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
-    #print (tfidf_matrix.shape)
-    
+
     #grab the closest match of the sentence based on cosine similarity
     from sklearn.metrics.pairwise import cosine_similarity
     c_similarity = []
@@ -82,41 +56,127 @@ def translate():
 
     #grab the closest match of the sentence based on cosine similarity
     index_max = np.argmax(c_similarity)
-    #print("The closest sentence match is: " + corpus[index_max+1])
-
+  
     #grab the matching sentence ID for the language using the identified text and convert it to a string
     sentence_id = from_language_sentences.loc[from_language_sentences['text'] == corpus[index_max+1]]['sentence_id'].values
     sentence_id = int(sentence_id)
     percent_match = str(round(np.amax(c_similarity),4)*100)
-    #print("That sentence is sentence ID: " + str(sentence_id))
-    #print("The value of the cosine similarity for the sentences is: " + str(np.amax(c_similarity)))
-        
+    my_return_df.ix[0, 'percent_match'] = percent_match
+	
     # read translations pandas df back from the pkl file
     pkl_file = open('translations.pkl', 'rb')
     translations_df = pickle.load(pkl_file)
     pkl_file.close()
 
-    #print(translations_df.head())
-    
-    #print(translations_df.loc[(translations_df['output_language_key'] == to_language)])
-    #print(translations_df.loc[(translations_df['output_language_key'] == to_language) & (translations_df['input_sentence_id'] == sentence_id)])
-            
     #get the resulting translations for the input sentence and desired language
-    translations = translations_df.loc[(translations_df['output_language_key'] == to_language) & (translations_df['input_sentence_id'] == sentence_id)]
+    translations = translations_df.loc[(translations_df['output_language_key'] == my_return_df['to_language'].values[0]) & (translations_df['input_sentence_id'] == sentence_id)]
     
-	#print(translations['input_text'].values[0])
-    #print(translations['output_text'].values[0])
-	
+    my_return_df.ix[0, 'sent'] = translations['input_text'].values[0]
+    my_return_df.ix[0, 'pst_text'] = translations['output_text'].values[0]
+    
     #Get Google Results
     from googletrans import Translator
     translator = Translator()
-    translated = translator.translate(sent, src=from_language, dest=to_language)
-    
-    return render_template('translated.html', googleResult = translated.text, resultFromText=translations['input_text'].values[0], resultToText=translations['output_text'].values[0], percentMatch=percent_match, resultOrigText = args['fromText'], resultFromLang=args['fromLang'], resultToLang=args['toLang'])
+    translated = translator.translate(my_return_df['orig_sent'].values[0], src=my_return_df['from_language'].values[0], dest=my_return_df['to_language'].values[0])
+    my_return_df.ix[0, 'google_text'] = translated.text
+	
+	
+    return
 
 
 
-def updateTranslations(from_language, new_from_text, to_language, new_to_text):
+
+@app.route('/translate', methods=['GET'])
+def translate():
+    import pandas as pd
+	
+    import warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning) 
+    # Params for model are in here!
+    args = request.args.to_dict()
+
+    button_id=''
+    button_id = args['submit_button']
+    print(button_id)
+	
+    #set input values
+    sent = args['fromText']
+    from_language=args['fromLang']
+    to_language=args['toLang']
+    google_text=args['googleText']
+    pst_text=args['pstText']
+	
+    my_return_obj = {'orig_sent': [sent], 'sent': [sent], 'from_language': [from_language], 'to_language': [to_language], 'google_text': [google_text], 'pst_text': [pst_text], 'percent_match': ["0"]}
+    my_return_df = pd.DataFrame(data=my_return_obj)
+	
+    if (button_id=="usePSTTranslation") :
+        updateTranslations(my_return_df,pst_text)
+		
+    if (button_id=="useGoogleTranslation") :
+        updateTranslations(my_return_df,google_text)
+	
+    populate_main(my_return_df)
+		
+    print("orig_sent: " + my_return_df['orig_sent'].values[0])
+    print("sent: " + my_return_df['sent'].values[0])
+    print("from_language: " + my_return_df['from_language'].values[0])
+    print("to_language: " + my_return_df['to_language'].values[0])
+    print("google_text: " + my_return_df['google_text'].values[0])
+    print("pst_text: " + my_return_df['pst_text'].values[0])
+    print("percent_match: " + my_return_df['percent_match'].values[0])
+	
+    return render_template('translated.html', googleResult = my_return_df['google_text'].values[0], resultFromText=my_return_df['sent'].values[0], resultToText=my_return_df['pst_text'].values[0], percentMatch=my_return_df['percent_match'].values[0], resultOrigText = my_return_df['orig_sent'].values[0], resultFromLang=my_return_df['from_language'].values[0], resultToLang=my_return_df['to_language'].values[0])
+
+	
+
+def updatePklFiles():
+    import mysql.connector
+    import numpy as np
+    import pandas as pd
+    import pickle
+    import sqlalchemy as sql
+    from sqlalchemy import create_engine
+    import os
+    import re
+    import warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+    # ====== Connection ====== #
+    # Connecting to mysql by providing a sqlachemy engine
+    engine = create_engine('mysql+pymysql://root:' + os.environ['pst_pwd'] + '@35.232.80.174/masters', echo=False)
+
+    #Export English sentences in a pkl file (EN=English)
+    df = pd.read_sql("select text, sentence_id from Sentences where language_key='EN'", engine)
+    output = open('sentences_EN.pkl', 'wb')
+    pickle.dump(df, output)
+    output.close()
+
+    #Export Portuguese sentences in a pkl file (PT=Portuguese)
+    df = pd.read_sql("select text, sentence_id from Sentences where language_key='PT'", engine)
+    output = open('sentences_PT.pkl', 'wb')
+    pickle.dump(df, output)
+    output.close()
+
+    #Export Hindi sentences in a pkl file (HI=Hindi)
+    df = pd.read_sql("select text, sentence_id from Sentences where language_key='HI'", engine)
+    output = open('sentences_HI.pkl', 'wb')
+    pickle.dump(df, output)
+    output.close()
+
+    #Export Translations sentences in a pkl file 
+    df = pd.read_sql("SELECT a.sentence_id as input_sentence_id, \
+    a.language_key as input_language_key, a.text as input_text, \
+    b.text as output_text, b.sentence_id as output_sentence_id, \
+    b.language_key as output_language_key FROM masters.Translations \
+    left join masters.Sentences a on a.sentence_id=Translations.sentence_id_1 \
+    left join masters.Sentences b on b.sentence_id=Translations.sentence_id_2", engine)
+    output = open('translations.pkl', 'wb')
+    pickle.dump(df, output)
+    output.close()
+
+    return
+
+def updateTranslations(my_return_df, new_to_text):
     print('in updateTranslations')
 	
     import mysql.connector
@@ -127,45 +187,58 @@ def updateTranslations(from_language, new_from_text, to_language, new_to_text):
     import sqlalchemy as sql
     from sqlalchemy import create_engine
     import re
+    import os
+	
+    import warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning) 
+	
+	
+    print("orig_sent: " + my_return_df['orig_sent'].values[0])
+    print("sent: " + my_return_df['sent'].values[0])
+    print("from_language: " + my_return_df['from_language'].values[0])
+    print("to_language: " + my_return_df['to_language'].values[0])
+    print("google_text: " + my_return_df['google_text'].values[0])
+    print("pst_text: " + my_return_df['pst_text'].values[0])
+    print("percent_match: " + my_return_df['percent_match'].values[0])
+    print("new_to_text: " + new_to_text)
 	
 	#The purpose of this code block is to find the test if our sentences are in the database
 
     #sanitize sentences
-    new_from_text = re.sub('[!@#$%;:.?()"\'^\{\}\[\]|\\\/<>=`~*&]', '', new_from_text).strip().lower()
     new_to_text = re.sub('[!@#$%;:.?()"\'^\{\}\[\]|\\\/<>=`~*&]', '', new_to_text).strip().lower()
-
+    my_return_df.ix[0, 'sent'] = re.sub('[!@#$%;:.?()"\'^\{\}\[\]|\\\/<>=`~*&]', '', my_return_df['sent'].values[0]).strip().lower()
 
     # ====== Connection ====== #
     # Connecting to mysql by providing a sqlachemy engine
-    engine = create_engine('mysql+pymysql://root:MBBmasters!@35.232.80.174/masters', echo=False)
+    engine = create_engine('mysql+pymysql://root:' + os.environ['pst_pwd'] + '@35.232.80.174/masters', echo=False)
 
     #Check for new from_text and insert if it is new
     df = pd.read_sql("select sentence_id from Sentences where language_key \
-    ='"+from_language+"' and text = '"+ new_from_text +"'", engine)
+    ='"+my_return_df['from_language'].values[0]+"' and text = '"+ my_return_df['sent'].values[0] +"'", engine)
 
     if len(df.index) == 0:
         #Insert due to no entries
-        data = [[new_from_text, from_language]] 
+        data = [[my_return_df['sent'].values[0], my_return_df['from_language'].values[0]]] 
         new_sentence_insert = pd.DataFrame(data, columns = ['text', 'language_key']) 
         new_sentence_insert.to_sql('Sentences', con=engine, if_exists='append', index = False)
         #Requery to get the value
         df = pd.read_sql("select sentence_id from Sentences where language_key \
-        ='"+from_language+"' and text = '"+ new_from_text +"'", engine)
+        ='"+my_return_df['from_language'].values[0]+"' and text = '"+ my_return_df['sent'].values[0] +"'", engine)
     
     from_sentence_id = df['sentence_id'].values[0]
     
     #Check for new to_text and insert if it is new
     df = pd.read_sql("select sentence_id from Sentences where language_key \
-    ='"+to_language+"' and text = '"+ new_to_text +"'", engine)
+    ='"+my_return_df['to_language'].values[0]+"' and text = '"+ new_to_text +"'", engine)
 
     if len(df.index) == 0:
         #Insert due to no entries
-        data = [[new_to_text, to_language]] 
+        data = [[new_to_text, my_return_df['to_language'].values[0]]] 
         new_sentence_insert = pd.DataFrame(data, columns = ['text', 'language_key']) 
         new_sentence_insert.to_sql('Sentences', con=engine, if_exists='append', index = False)
         #Requery to get the value
         df = pd.read_sql("select sentence_id from Sentences where language_key \
-        ='"+to_language+"' and text = '"+ new_to_text +"'", engine)
+        ='"+my_return_df['to_language'].values[0]+"' and text = '"+ new_to_text +"'", engine)
 
     to_sentence_id = df['sentence_id'].values[0]
 	
@@ -200,6 +273,8 @@ def updateTranslations(from_language, new_from_text, to_language, new_to_text):
     sentence_id_2 ="+str(from_sentence_id)+" and sentence_id_1 = "+ str(to_sentence_id), engine)
 
     #print(translation_2_df)
+	
+    updatePklFiles()
 	
     return 
 
